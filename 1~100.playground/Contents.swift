7b5e106efa60db4dca13c986d5918d388d295d6b
20161104599 <wb20161104599@123.local>
2018-09-12 11:32:34 +0800

    100
    1~100

---
+++ 1~100.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+var i=0
+var sum=0
+for i in 0...100{
+    sum=sum+i
+}
+print(sum)
+
+
+
+var str = "Hello, playground"
---
+++ 9.12/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  9.12
+//
+//  Created by 16wb395 on 2018/9/12.
+//  Copyright © 2018年 16wb395. All rights reserved.
+//
+
+import UIKint
+import PlaygroundSupport
+
+
+
+print("Hello, World!")
+
